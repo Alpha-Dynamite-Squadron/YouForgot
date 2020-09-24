4711136320b4bad3012405553c75919becdb262f
Ryan Dimitri Ramos <dimitiriramos99@gmail.com>
2020-09-23 18:36:30 -0700

    Added a package dependency checker to test.js

--- server/test/test.js
+++ server/test/test.js
@@ -1,7 +1,53 @@
 var should = require('should');
+var depcheck = require('depcheck');
+
 
 describe('Initial Test', function() { 
     it('is running', function() {
         true.should.equal(true);
+    });
+});
+
+describe('Dependency Check Test', function() { 
+    it('is running', function() {
+        const options = {
+            ignoreBinPackage: false, // ignore the packages with bin entry
+            skipMissing: false, // skip calculation of missing dependencies
+            ignorePatterns: [
+              // files matching these patterns will be ignored
+              'sandbox',
+              'dist',
+              'bower_components',
+            ],
+            ignoreMatches: [
+              // ignore dependencies that matches these globs
+              'grunt-*',
+            ],
+            parsers: {
+              // the target parsers
+              '*.js': depcheck.parser.es6,
+              '*.jsx': depcheck.parser.jsx,
+            },
+            detectors: [
+              // the target detectors
+              depcheck.detector.requireCallExpression,
+              depcheck.detector.importDeclaration,
+            ],
+            specials: [
+              // the target special parsers
+              depcheck.special.eslint,
+              depcheck.special.webpack,
+            ],
+          };
+        depcheck(process.cwd(), options, (unused) => {
+           
+            let isEmpty = false;
+            if(JSON.stringify(unused.missing) == JSON.stringify({})){
+                isEmpty = true;
+            }else{
+                console.log(unused.missing);
+            }
+            isEmpty.should.equal(true);
+          });
     });
 });
