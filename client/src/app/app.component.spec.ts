769d633bbaf54b499d3d8db3ed8dec83bdc760a3
SneakySalmander <nickstewart11@hotmail.com>
2020-09-23 17:24:16 -0700

    Moved source javascript into its on folder for backend. Modified lint script for server.

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'you-forgot-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('you-forgot-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('you-forgot-app app is running!');
-  });
 });
