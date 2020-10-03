75185efc066ee7ec8f77dbe9964586768a920b8e
SneakySalmander <nickstewart11@hotmail.com>
2020-10-03 14:46:50 -0700

    Made significant changes to angular configuration files and package.json. Also added styling templates from third-party sources.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, NavigationStart } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'you-forgot-app',
+    templateUrl: './app.component.html'
 })
-export class AppComponent {
 
+export class AppComponent implements OnInit {
+  private _router: Subscription;
+
+  constructor( private router: Router ) {
+  }
+    ngOnInit() {
+      this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
+        const body = document.getElementsByTagName('body')[0];
+        const modalBackdrop = document.getElementsByClassName('modal-backdrop')[0];
+        if (body.classList.contains('modal-open')) {
+          body.classList.remove('modal-open');
+          modalBackdrop.remove();
+        }
+      });
+    }
 }
