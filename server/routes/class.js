66656b59aff9ef6c016d2c82e2986196bfaebe31
kennyklee123 <kennyklee123@gmail.com>
2020-10-26 15:45:23 -0700

    need to do routes

---
+++ server/routes/class.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { getClasses, getAllAssingments, getAssignments, createAssignment, createCourse } from '../controllers/class.js';
+
+const router = express.Router();
+
+
+
+
+
