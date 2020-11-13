618077391fd6ac03c26999dd54f88575295a1c89
SneakySalmander <nickstewart11@hotmail.com>
2020-11-13 13:01:02 -0800

    Added separate unique component for personal assignments viewed under myassignments.

---
+++ client/src/app/components/personal-assignment/personal-assignment.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-assignment',
+  templateUrl: './personal-assignment.component.html',
+  styleUrls: ['./personal-assignment.component.css']
+})
+export class PersonalAssignmentComponent implements OnInit {
+
+  icons: string[] = [
+    'assignment',
+    'book',
+    'science',
+    'calculate'];
+  assignmentName: string[] = [
+    'Challenging Problem Homework #6',
+    'CriminalIntent Application Chapters 9-13',
+    'Homework#7 Problems 13-26 Odd',
+    'Argumentative Essay'];
+  assignmentDescription: string[] = [
+    'Complete the homework posted on Black Board',
+    'Submit the assignment as soon as possible',
+    'This assignment will prepare you for final',
+    'Do not wait to start this assignment'];
+  assignmentDueDate: string[] = [
+    '10/29/2020',
+    '11/7/2020',
+    '12/13/2020',
+    '11/13/2020'];
+  assignmentGradeOptions: boolean[] = [true, false];
+
+  selectedIcon: string;
+  selectedAssignmentName: string;
+  selectedAssignmentDesc: string;
+  selectedAssignmentDueDate: string;
+  selectedGradingOption: boolean;
+
+  forgotCount: number;
+  rememberedCount: number;
+  assignmentPostedDate: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.selectedIcon = this.icons[Math.floor(Math.random() * this.icons.length)];
+    this.selectedAssignmentName = this.assignmentName[Math.floor(Math.random() * this.assignmentName.length)];
+    this.selectedAssignmentDesc = this.assignmentDescription[Math.floor(Math.random() * this.assignmentDescription.length)];
+    this.selectedAssignmentDueDate = this.assignmentDueDate[Math.floor(Math.random() * this.assignmentDueDate.length)];
+    this.selectedGradingOption = this.assignmentGradeOptions[Math.floor(Math.random() * this.assignmentGradeOptions.length)];
+    this.forgotCount = Math.floor(Math.random() * 100);
+    this.rememberedCount = Math.floor(Math.random() * 100);
+    this.assignmentPostedDate = this.selectedAssignmentDueDate;
+  }
+
+}
