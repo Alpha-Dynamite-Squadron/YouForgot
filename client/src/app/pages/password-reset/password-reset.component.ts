71b523f2fb4d9eda591b5ec51d11fbcc25526b95
SneakySalmander <nickstewart11@hotmail.com>
2020-11-13 14:43:48 -0800

    Password Reset Component redesigned and improved to follow reactive approach. Styling and other slight changes made as well.

--- client/src/app/pages/password-reset/password-reset.component.ts
+++ client/src/app/pages/password-reset/password-reset.component.ts
@@ -1,6 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators, FormGroup } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { FormBuilder, AbstractControl } from '@angular/forms';
+import { PasswordValidation } from '../../forms/validationforms/password-validator';
 import { AuthenticationService } from 'src/authentication.service';
+import { Router } from '@angular/router';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-password-reset',
@@ -8,17 +19,54 @@ import { AuthenticationService } from 'src/authentication.service';
   styleUrls: ['./password-reset.component.css']
 })
 export class PasswordResetComponent implements OnInit {
-
-  password: string = '';
-  confirmPass: string = '';
+  
   validConfirmPasswordReset: boolean = false;
   validPasswordReset: boolean = false;
-  
-  constructor(
-    private authService: AuthenticationService,
-    private router: Router) { }
+  validTextType: boolean = false;
 
-  ngOnInit(): void {
+  matcher = new MyErrorStateMatcher();
+  passwordResetForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthenticationService) { }
+
+    ngOnInit() {
+      this.passwordResetForm = this.formBuilder.group({
+        password: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+        confirmPassword: ['', Validators.required],
+      }, {
+        validator: PasswordValidation.MatchPassword
+      });
+    }
+
+  isFieldValid(form: FormGroup, field: string) {
+    return !form.get(field).valid && form.get(field).touched;
+  }
+
+  displayFieldCss(form: FormGroup, field: string) {
+    return {
+      'has-error': this.isFieldValid(form, field),
+      'has-feedback': this.isFieldValid(form, field)
+    };
+  }
+
+  onType() {
+    if (this.passwordResetForm.valid) {
+    } else {
+      this.validateAllFormFields(this.passwordResetForm);
+    }
+  }
+  validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsTouched({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    });
   }
 
   passwordValidationReset(e) {
@@ -29,16 +77,17 @@ export class PasswordResetComponent implements OnInit {
     }
   }
   confirmPasswordValidationReset(e) {
-    if (this.password === e) {
+    if (this.passwordResetForm.controls['password'].value === e) {
       this.validConfirmPasswordReset = true;
     } else {
       this.validConfirmPasswordReset = false;
     }
   }
-
+ 
   resetPassword() {
-    console.log('Finishing Password Reset...');
-    console.log("Resetting Password to : " + this.confirmPass);
+    console.log('Form Submitted.');
+    console.log('Submission Valid, sending POST Request: ' + JSON.stringify(this.passwordResetForm.value));
+    alert('Submission Valid, sending POST Request: ' + JSON.stringify(this.passwordResetForm.value));
     //Add endpoint code here
     this.router.navigateByUrl('/login');
   }
