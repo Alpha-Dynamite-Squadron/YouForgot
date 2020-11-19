e47e67f3981f058bc55f2334df96b3b006e0ae40
SneakySalmander <nickstewart11@hotmail.com>
2020-11-18 16:30:21 -0800

    Edited avatar assets.

--- server/routes/class.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getClasses, getAllAssingments, getAssignments, createAssignment, createCourse } from '../controllers/class.js';
-
-const router = express.Router();
-
-
-
-
-
